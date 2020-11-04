69dbb551ce6192f3c2bb86d0078bf31d25bfbbe8
danhtungk3 <67349525+danhtungk3@users.noreply.github.com>
2020-11-04 22:57:02 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Title</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<style>
+		ul#menu li {
+			display: inline;
+		}
+
+		#menu li {
+			font-size: 12px;
+		}
+
+		ul#language li {
+			display: inline;
+		}
+
+		.menu a:hover,
+		a:link {
+			text-decoration: none;
+			color: #fff;
+		}
+
+		.menu a:link {
+			text-decoration: none;
+		}
+
+		.container-fruid {
+			background-color: #006699;
+		}
+		
+	</style>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+	<div>
+		<div class="container-fruid">
+			<div class="container ">
+				<div class="col-md-12 d-flex">
+
+					<div class="menu col-md-8 ">
+						<ul id="menu">
+							<li><a href="trangchu.php">Trang chủ </a></li>
+							<li> | </li>
+							<li><a href="#">Lịch tuần</li>
+							<li> | </li>
+							<li><a href="#">Kết quả tuyển sinh</li>
+							<li> | </li>
+							<li><a href="#">Học liệu</li>
+							<li> | </li>
+							<li><a href="#">Hướng dẫn sử dụng website</li>
+							<li> | </li>
+							<li><a href="#">Liên hệ </li>
+							<li> | </li>
+							<li><a href="#">Đăng nhập</li>
+						</ul>
+					</div>
+
+					<div class="col-md-2"></div>
+
+					<div class="col-md-3">
+						<ul id="language">
+							<li style="width:32px;"><a class="ui-link-white lang-vi">
+									<img src="img/icon-lang-vi.png" alt="">
+								</a>
+							</li>
+							<li><a class="ui-link-white lang-vi">
+									<img src="img/en.png" alt="">
+								</a>
+							</li>
+							<li><a class="ui-link-white lang-vi">
+									<img src="img/fr.png" alt="">
+								</a>
+							</li>
+						</ul>
+					</div>
+
+				</div>
+
+				<div class="logo">
+					<div class="col-md-2">
+						<img src="img/logo.png">
+					</div>
+					
+				</div>
+
+			</div>
+		</div>
+  </div>
+  </div
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
